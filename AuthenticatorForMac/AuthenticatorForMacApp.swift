13413608483e0ff96d14f109e19294d694601de7
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2023-05-12 01:23:01 +0900

    Initial Commit

---
+++ AuthenticatorForMac/AuthenticatorForMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticatorForMacApp.swift
+//  AuthenticatorForMac
+//
+//  Created by iwashita on 2023/05/12.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticatorForMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
